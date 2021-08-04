298366df362fec07a704d5ead0ae3e88874150c7
negrulu <61474831+negrulu@users.noreply.github.com>
2021-08-04 15:13:04 -0500

    Tarea 17: Ejercicios de refuerzo vectores

---
+++ Tarea 17/src/RefuerzodeVectores/MainVectores.java
@@ -0,0 +1,82 @@
+package RefuerzodeVectores;
+
+public class MainVectores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1
+		int X[] = new int [8];
+		
+		//2
+		char Y[] = new char [3];
+		Y[2]='A';
+		
+		//3
+		int A[] = new int [5];
+		A[0]=25;
+		A[3]=49;
+		A[2]=A[0];
+		
+		//4
+		int B[] = new int [10];
+		B[0]=A[3];
+		
+		//5
+		int Z[] = new int [12];
+		Z[0]=1;
+		Z[1]=2;
+		Z[2]=3;
+		Z[3]=4;
+		Z[4]=5;
+		Z[5]=6;
+		Z[6]=7;
+		Z[7]=8;
+		Z[8]=9;
+		Z[9]=10;
+		Z[10]=11;
+		Z[11]=12;
+		Z[12]=13;
+		
+		//6
+		//En el ejercicio dice que el vector sea Y pero pongo U
+		//por que ya tenemos una variable 
+		int U[] = new int [10];
+		U[1]=1;
+		U[3]=2;
+		U[5]=3;
+		U[7]=4;
+		U[9]=5;
+		
+		//7
+		char H[] = new char [10];
+		H[1]='A';
+		H[3]='B';
+		H[5]='C';
+		H[7]='D';
+		H[9]='E';
+		
+		//8
+		int F[] = new int [10];
+		F[0]=H[3];
+		F[3]=H[2];
+		F[4]=H[6];
+		F[7]=H[0];
+		F[8]=H[1];
+		F[9]=H[9];
+		
+		//9
+		int L[] = new int [15];
+		L[0]=3+5+7+9/4;
+		L[4]=5+7+8+15;
+		L[7]=6*7*3;
+		L[10]=10/2;
+		
+		//10
+		int N[] = new int [12];
+		N[0]=3+10+9/3;
+		N[4]=7+8+10;
+		N[12]=5*7*3;
+		N[20]=7+-3+8;
+	}
+
+}
